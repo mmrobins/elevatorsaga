854286770f05c92b690789db9561f71249a319d0
Matt Robinson <matt@mattrobinson.net>
2015-01-23 10:54:15 -0800

    Convert 4 space to 2 space for readability

--- level6.js
+++ level6.js
@@ -1,38 +1,38 @@
 // sucks for wait time, but OK for # moves
 // works for level 7 too
 {
-    init: function(elevators, floors) {
-        var rebalanceQueue = function(e) {
-            console.log("Before rebalance: " + e.destinationQueue)
-            e.destinationQueue = _.uniq(e.destinationQueue)
-            e.checkDestinationQueue()
-            console.log("After rebalance: " + e.destinationQueue)
-        };
-        window.elevators = elevators;
-        window.floors = floors;
-        _.each(floors, function(f) {
-            _.each(['up_button_pressed', 'down_button_pressed'], function(event) {
-              f.on(event, function() {
-                  if (_.find(elevators, function(e) { return _.include(e.destinationQueue, f.level) })) {
-                      console.log("elevator already en route");
-                      return
-                  };
+  init: function(elevators, floors) {
+    var rebalanceQueue = function(e) {
+      console.log("Before rebalance: " + e.destinationQueue)
+      e.destinationQueue = _.uniq(e.destinationQueue)
+      e.checkDestinationQueue()
+      console.log("After rebalance: " + e.destinationQueue)
+    };
+    window.elevators = elevators;
+    window.floors = floors;
+    _.each(floors, function(f) {
+      _.each(['up_button_pressed', 'down_button_pressed'], function(event) {
+        f.on(event, function() {
+          if (_.find(elevators, function(e) { return _.include(e.destinationQueue, f.level) })) {
+            console.log("elevator already en route");
+            return
+          };
 
-                  e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
-                  e.goToFloor(f.level);
-                  rebalanceQueue(e);
+          e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
+          e.goToFloor(f.level);
+          rebalanceQueue(e);
 
-              });
-            });
         });
-        elevators.forEach(function(e) {
-            e.on("floor_button_pressed", function(floorNum) {
-                e.goToFloor(floorNum);
-                rebalanceQueue(e);
-            });
-        });
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+      });
+    });
+    elevators.forEach(function(e) {
+      e.on("floor_button_pressed", function(floorNum) {
+        e.goToFloor(floorNum);
+        rebalanceQueue(e);
+      });
+    });
+  },
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
 }
