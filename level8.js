4c67768af6f7e5d561f53bb45f4e5ac02a7c2035
Matt Robinson <matt@mattrobinson.net>
2015-01-23 11:07:05 -0800

    Extract closest elevator to function

--- level8.js
+++ level8.js
@@ -1,19 +1,25 @@
 // only had to remove a tiny bit of code from 6 to decrease wait time
 // which also increases # moves
+// works for level 9 also
 {
   init: function(elevators, floors) {
     var rebalanceQueue = function(e) {
-      console.log("Before rebalance: " + e.destinationQueue)
+      // console.log("Before rebalance: " + e.destinationQueue)
       e.destinationQueue = _.uniq(e.destinationQueue)
       e.checkDestinationQueue()
-      console.log("After rebalance: " + e.destinationQueue)
+      // console.log("After rebalance: " + e.destinationQueue)
+    };
+    var closestElevator = function(els, floor) {
+       es = _.sortBy(els, function(ele) {return Math.abs(floor - ele.currentFloor())});
+       console.log(es);
+       return es[0];
     };
     window.elevators = elevators;
     window.floors = floors;
     _.each(floors, function(f) {
       _.each(['up_button_pressed', 'down_button_pressed'], function(event) {
         f.on(event, function() {
-          e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
+          e = closestElevator(elevators, f.level);
           e.goToFloor(f.level);
           rebalanceQueue(e);
 
