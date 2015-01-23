8c3bb2ef3b3ad4f0454be73b0a493d848c988d6c
Matt Robinson <matt@mattrobinson.net>
2015-01-23 10:53:53 -0800

    Refactor button events to be DRY

--- level6.js
+++ level6.js
@@ -11,26 +11,18 @@
         window.elevators = elevators;
         window.floors = floors;
         _.each(floors, function(f) {
-            f.on('up_button_pressed', function() {
-                if (_.find(elevators, function(e) { return _.include(e.destinationQueue, f.level) })) {
-                    console.log("elevator already en route");
-                    return
-                };
+            _.each(['up_button_pressed', 'down_button_pressed'], function(event) {
+              f.on(event, function() {
+                  if (_.find(elevators, function(e) { return _.include(e.destinationQueue, f.level) })) {
+                      console.log("elevator already en route");
+                      return
+                  };
 
-                e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
-                e.goToFloor(f.level);
-                rebalanceQueue(e);
+                  e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
+                  e.goToFloor(f.level);
+                  rebalanceQueue(e);
 
-            });
-            f.on('down_button_pressed', function() {
-                if (_.find(elevators, function(e) { return _.include(e.destinationQueue, f.level) })) {
-                    console.log("elevator already en route");
-                    return
-                };
-                e = _.sortBy(elevators, function(ele){return Math.abs(f.level - ele.currentFloor())})[0];
-                e.goToFloor(f.level);
-                e.destinationQueue = _.uniq(e.destinationQueue);
-                rebalanceQueue(e);
+              });
             });
         });
         elevators.forEach(function(e) {
